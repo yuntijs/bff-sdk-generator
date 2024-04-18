12d5322c1468937b2af6bc30cb8544fa5bbd18e3
Carrotzpc <zhang.pc3@gmail.com>
2024-04-18 15:32:33 +0800

    ci(sdk): add CONFIG_USE_SWR_INFINITE support

--- codegen.ts
+++ codegen.ts
@@ -1,6 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const endpoint = process.env.GRAPH_API_ENDPOINT;
+const useSWRInfiniteConfig = process.env.CONFIG_USE_SWR_INFINITE || '';
 
 const config: CodegenConfig = {
   schema: endpoint,
@@ -20,6 +21,7 @@ const config: CodegenConfig = {
   },
   config: {
     autogenSWRKey: true,
+    useSWRInfinite: useSWRInfiniteConfig.split(',')
   },
 };
 
